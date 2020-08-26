da4de5199211e4ea16975c294cea59b0235bffac
soligits <m.soltani.252@gmail.com>
2020-08-27 03:05:14 +0430

    Added a test GET endpoint to the server.

--- server.go
+++ server.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	server := gin.Default()
+
+	server.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "OK!",
+		})
+	})
+
+	server.Run(":8080")
 }
